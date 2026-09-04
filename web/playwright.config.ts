41865941189d1c4bac3b4bcf29855c0c4bdab2a5
James Ding <git@twango.dev>
2026-09-04 10:29:57 -0700

    ci(web): test the deployed static artifact

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 		trace: 'on-first-retry'
 	},
 	webServer: {
-		command: 'bun run preview --host 127.0.0.1 --port 4173',
+		command: 'bunx wrangler dev --local --ip 127.0.0.1 --port 4173',
 		url: 'http://127.0.0.1:4173',
 		reuseExistingServer: !process.env.CI
 	},
